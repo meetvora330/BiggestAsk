5d0791af87faabd21958a5dede0dfc7547861ee0
shah <shah272728@gmail.com>
2022-08-09 13:58:25 +0530

    App notification setup

---
+++ app/src/main/java/com/biggestAsk/notification/TheBiggestAskMessagingService.kt
@@ -0,0 +1,39 @@
+package com.biggestAsk.notification
+
+import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.biggestAsk.util.NotificationUtils
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class TheBiggestAskMessagingService : FirebaseMessagingService() {
+    private var broadcaster: LocalBroadcastManager? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        broadcaster = LocalBroadcastManager.getInstance(this)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        try {
+            Log.d("Notification", message.toString() + message.data.toString())
+            if (message.notification?.title!!.isNotEmpty()) {
+                makeClNotification(message)
+            }
+        } catch (t: Throwable) {
+            t.printStackTrace()
+        }
+    }
+
+    private fun makeClNotification(message: RemoteMessage) {
+        NotificationUtils.buildSimpleNotification(this, message)
+    }
+
+    override fun onNewToken(token: String) {
+        Log.d("Notification token", token)
+//        CleverTapAPI.getDefaultInstance(this)?.pushFcmRegistrationId(token, true)
+//        FreshchatManager.onNewFCMToken(this, token)
+//        AppsFlyerLib.getInstance().updateServerUninstallToken(this, token)
+    }
+}
---
+++ app/src/main/java/com/biggestAsk/util/NotificationUtils.kt
@@ -0,0 +1,81 @@
+package com.biggestAsk.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.biggestAsk.ui.MainActivity
+import com.example.biggestAsk.R
+import com.google.firebase.messaging.RemoteMessage
+
+
+object NotificationUtils {
+
+    fun buildSimpleNotification(context: Context, message: RemoteMessage) {
+        val data = message.notification
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            context, 1, intent, 0
+        )
+
+        val channelId = "111"
+
+        val titleText = data?.title
+        val contentText = data?.body
+        val symActionChat = R.drawable.logo_setting_privacy_policy
+
+        val myBitMap = BitmapFactory.decodeResource(
+            context.resources,
+            R.mipmap.ic_launcher
+        )
+
+        val nb = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationCompat.Builder(context, channelId)
+        } else {
+            NotificationCompat.Builder(context)
+        }
+
+        nb.apply {
+            setContentIntent(pendingIntent)
+            setSmallIcon(symActionChat)
+            setContentTitle(titleText)
+            color = ContextCompat.getColor(
+                context,
+                R.color.blue_light
+            )
+            setContentText(contentText)
+            myBitMap
+            setLargeIcon(myBitMap)
+            setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(contentText)
+            )
+            setGroup("GROUP_KEY")
+            priority = NotificationCompat.PRIORITY_MAX
+            setAutoCancel(true)
+        }
+
+        val mNotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "channelName",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            mNotificationManager?.createNotificationChannel(channel)
+            channelId.let { nb.setChannelId(it) }
+        }
+        mNotificationManager?.notify(
+            titleText.hashCode(),
+            nb.build()
+        )
+    }
+
+}
